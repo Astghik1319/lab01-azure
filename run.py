84c763639528f8a05476f7a8beb1bc5ca95ac851
Astghik1319 <74616021+Astghik1319@users.noreply.github.com>
2021-06-12 11:17:13 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import abort, send_from_directory
+from flask import Flask, render_template, request
+from AzureDB import AzureDB
+
 import os
 app = Flask(__name__)
 
@@ -47,3 +50,27 @@ def error_denied():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+@app.route("/")
+def index():
+ msg = Message('Hello', sender = 'yourId@gmail.com', recipients = ['someone1@gmail.com'])
+ msg.body = "Hello Flask message sent from Flask-Mail"
+ mail.send(msg)
+ return "Sent"
+if __name__ == '__main__':
+ app.run(debug = True)
+ 
+ @app.route('/')
+def hello():
+ with AzureDB() as a:
+ data = a.azureGetData()
+ return render_template("result.html", data = data)
+if __name__ == '__main__':
+ app.run(debug=True)
